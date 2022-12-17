0ccc32b6de98b7dd90eee75b641d0d9e1f6b6940
Dastan Kadylbekov <dastan.962601@gmail.com>
2022-12-17 18:05:49 +0600

    build new app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <nav></nav>
   </div>
 </template>
 
@@ -23,10 +19,6 @@ nav {
   a {
     font-weight: bold;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
 </style>
